72bc9ef4bc4832ce72401df377130f6fa4f3d535
Christopher de Oliveira <christopherforwork@gmail.com>
2021-04-20 13:20:28 -0400

    🚧 (CRUD): Developed Animal CRUD
    - Observation: I did not find where to put the id_fazenda, and because
    of that I am treating this field as nullable. There is no another
    fazenda object in this challenge project.

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  pessoaController
+  pessoaController,
+  animalController
 } = require('./app/controllers');
 
 const {
-  pessoaValidator
+  pessoaValidator,
+  animalValidator
 } = require('./app/validators');
 
 // Know if server is on by requesting this route
@@ -51,5 +53,28 @@ router.post('/pessoa/',
 /* 
  * Animal related routes
  */
+router.get('/animal/:id',
+  animalValidator.readById,
+  animalController.readById,
+);
+
+router.put('/animal/:id',
+  animalValidator.update,
+  animalController.update,
+);
+
+router.delete('/animal/:id',
+  animalValidator.delete,
+  animalController.delete,
+);
+
+router.get('/animal/',
+  animalController.readAll,
+);
+
+router.post('/animal/',
+  animalValidator.create,
+  animalController.create,
+);
 
 module.exports = router;
